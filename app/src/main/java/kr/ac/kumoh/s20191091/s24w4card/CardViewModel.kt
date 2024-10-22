4971a7e92d38ffb40bb0b69907a4d2ba8a1ed389
= <=>
2024-10-22 11:40:00 +0900

    🎨 Set 사용

--- app/src/main/java/kr/ac/kumoh/s20191091/s24w4card/CardViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20191091/s24w4card/CardViewModel.kt
@@ -15,28 +15,38 @@ class CardViewModel : ViewModel() {//()도 붙여야함
     val cards: LiveData<IntArray>
         get() = _cards //복사를 해서 줌(원본에는 영향x)
 
-    fun shuffle(){
-        // var로 변경, size를 0으로 변경
-        var newCards = IntArray(0)
-//        val newCards = IntArray(5) { 0 }
-
-// (1)       for(i in _cards.indices)
-//            _cards[i] = Random.nextInt(52)
-
-//        for (i in newCards.indices)
-////         TODO: 중복 및 정렬 처리
-//            newCards[i] = Random.nextInt(52)
+//    fun shuffle(){
+//        // var로 변경, size를 0으로 변경
+//        var newCards = IntArray(0)
+////        val newCards = IntArray(5) { 0 }
+//
+//// (1)       for(i in _cards.indices)
+////            _cards[i] = Random.nextInt(52)
+//
+////        for (i in newCards.indices)
+//////
+////            newCards[i] = Random.nextInt(52)
+////
+////        _cards.value = newCards
+//
+//        while (newCards.size < 5) {
+//            val num = Random.nextInt(52)
+//            if (!newCards.contains(num))
+//                newCards = newCards.plus(num)
+//        }
+//
+//        newCards.sort()
 //
 //        _cards.value = newCards
+//    }
+
+    fun shuffle() {
+        val newCards = mutableSetOf<Int>()
 
         while (newCards.size < 5) {
-            val num = Random.nextInt(52)
-            if (!newCards.contains(num))
-                newCards = newCards.plus(num)
+            newCards.add(Random.nextInt(52))
         }
 
-        newCards.sort()
-
-        _cards.value = newCards
+        _cards.value = newCards.sorted().toIntArray()
     }
 }
