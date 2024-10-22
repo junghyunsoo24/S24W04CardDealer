d9e11a0a0f32984d95da92fc8a4034608e4083c3
= <=>
2024-10-22 11:34:18 +0900

    ♻️ 중복 방지 및 정렬

--- app/src/main/java/kr/ac/kumoh/s20191091/s24w4card/CardViewModel.kt
+++ app/src/main/java/kr/ac/kumoh/s20191091/s24w4card/CardViewModel.kt
@@ -16,14 +16,26 @@ class CardViewModel : ViewModel() {//()도 붙여야함
         get() = _cards //복사를 해서 줌(원본에는 영향x)
 
     fun shuffle(){
-        val newCards = IntArray(5) { 0 }
+        // var로 변경, size를 0으로 변경
+        var newCards = IntArray(0)
+//        val newCards = IntArray(5) { 0 }
 
 // (1)       for(i in _cards.indices)
 //            _cards[i] = Random.nextInt(52)
 
-        for (i in newCards.indices)
-//         TODO: 중복 및 정렬 처리
-            newCards[i] = Random.nextInt(52)
+//        for (i in newCards.indices)
+////         TODO: 중복 및 정렬 처리
+//            newCards[i] = Random.nextInt(52)
+//
+//        _cards.value = newCards
+
+        while (newCards.size < 5) {
+            val num = Random.nextInt(52)
+            if (!newCards.contains(num))
+                newCards = newCards.plus(num)
+        }
+
+        newCards.sort()
 
         _cards.value = newCards
     }
